b78b192cd11e19006b64630f9e7d8cb238a1a13a
David Figueiredo <150687060+G0DQwerty@users.noreply.github.com>
2024-05-04 17:41:26 +0100

    Unit5_Part1
    Made all the steps for the part 1 of unit 5

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
